e126ac9387bd1f881b2bc9ba074acf3c537c7379
leeli <>
2021-07-09 17:22:41 +0800

    update party left nav

--- component/frontend/app/controllers/frontend/parties_controller.rb
+++ component/frontend/app/controllers/frontend/parties_controller.rb
@@ -4,12 +4,22 @@ module Frontend
   class PartiesController < ApplicationController
     # before_action :set_party, only: [:show, :edit, :update, :destroy]
     before_action :set_party_navs
+    before_action :set_default_return_path
+
 
     layout 'frontend/party_application'
 
     # GET /parties
     def index
       # @parties = Party.all
+    end
+
+    def profile
+      @return_path = parties_path
+    end
+
+    def brand_detail
+      @return_path = parties_brand_path
     end
 
     private
@@ -26,34 +36,34 @@ module Frontend
       def set_party_navs
         @party_navs = [
           {path: parties_profile_path,
-            img_path: 'media/images/party/navs/01.png',
-            active_img_path: 'media/images/party/navs/1.png',
-            big_img_path: 'media/images/party/navs/001.png'
+            img_path: 'media/images/party/navs/1.png',
+            active_img_path: 'media/images/party/navs/01.png',
+            big_img_path: 'media/images/party/navs/title/1.png'
           },
           {path: parties_brand_path,
-            img_path: 'media/images/party/navs/02.png',
-            active_img_path: 'media/images/party/navs/2.png',
-            big_img_path: 'media/images/party/navs/002.png'
+            img_path: 'media/images/party/navs/2.png',
+            active_img_path: 'media/images/party/navs/02.png',
+            big_img_path: 'media/images/party/navs/title/2.png'
           },
           {path: parties_lead_path,
-            img_path: 'media/images/party/navs/03.png',
-            active_img_path: 'media/images/party/navs/3.png',
-            big_img_path: 'media/images/party/navs/003.png'
+            img_path: 'media/images/party/navs/3.png',
+            active_img_path: 'media/images/party/navs/03.png',
+            big_img_path: 'media/images/party/navs/title/3.png'
           },
           {path: parties_society_path,
-            img_path: 'media/images/party/navs/04.png',
-            active_img_path: 'media/images/party/navs/4.png',
-            big_img_path: 'media/images/party/navs/004.png'
+            img_path: 'media/images/party/navs/4.png',
+            active_img_path: 'media/images/party/navs/04.png',
+            big_img_path: 'media/images/party/navs/title/4.png'
           },
           {path: parties_service_path,
-            img_path: 'media/images/party/navs/05.png',
-            active_img_path: 'media/images/party/navs/5.png',
-            big_img_path: 'media/images/party/navs/005.png'
+            img_path: 'media/images/party/navs/5.png',
+            active_img_path: 'media/images/party/navs/05.png',
+            big_img_path: 'media/images/party/navs/title/5.png'
           },
           {path: parties_honor_path,
-            img_path: 'media/images/party/navs/06.png',
-            active_img_path: 'media/images/party/navs/6.png',
-            big_img_path: 'media/images/party/navs/006.png'
+            img_path: 'media/images/party/navs/6.png',
+            active_img_path: 'media/images/party/navs/06.png',
+            big_img_path: 'media/images/party/navs/title/6.png'
           },
           {path: parties_video_path,
             img_path: 'media/images/party/navs/07.png',
@@ -68,7 +78,10 @@ module Frontend
             @active_party_nav = hash
           end
         end
+      end
 
+      def set_default_return_path
+        @return_path ||= parties_profile_path
       end
 
   end
