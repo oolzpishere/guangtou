9750aa9761be8ab497f3277b4a35f31568e810b7
leeli <>
2021-11-22 16:09:53 +0800

    first change parties/navigator

--- app/packs/src/parties/navigator.js
+++ app/packs/src/parties/navigator.js
@@ -33,6 +33,8 @@ $(document).on("turbolinks:load", function() {
       partyIndexSwiper.slideToLoop(id - 1)
     });
 
+    // gaungxi branches
+
     $('.point-container').on('click touchstart', function(){
       var item = $(this).parents('.industry-point-container')
       var id = $(this).data('id')
@@ -42,14 +44,15 @@ $(document).on("turbolinks:load", function() {
       $('.industry-detail-img-container').removeClass('active')
 
       item.addClass('active')
-      filterItemsById( $('.industry-detail-img-container'), id).addClass('active')
+      filterItems( $('.industry-detail-img-container'), 'id', id).addClass('active')
       return false
     })
 
     $('.detail-tag').on('click touchstart', function(){
       $('.local-detail-container').removeClass('active')
-      var id = $(this).data('id')
-      filterItemsById( $('.local-detail-container'), id).addClass('active')
+      var tag_name = $(this).data('tag-name')
+
+      filterItems( $('.local-detail-container'), "tag-name", tag_name).addClass('active')
 
       return false
     })
@@ -60,12 +63,13 @@ $(document).on("turbolinks:load", function() {
       return false
     })
 
-    function filterItemsById( items, id ){
+    function filterItems( items, data_tag, value ){
       var item = items.filter(function() {
-        return $(this).data("id") == id;
+        return $(this).data( data_tag ) == value;
       });
       return item
     }
+
 
 
   }
@@ -73,3 +77,18 @@ $(document).on("turbolinks:load", function() {
 
 
 });
+
+class GxPartyBranches {
+  constructor() {
+    // this.sequenceArray = [];
+    // this.initSequenceArray()
+    // this.sortSequence();
+  }
+
+  // getLabel() {
+  //   var label = 'sequence' + this.$item.data('sequence-id')
+  //   // var label = 'sequence' + $(item).data('sequence-id')
+  //   return label
+  // };
+
+}
